9dcf5df730cc27bb1ef9d3b70b713b6f2a5c4c5c
amit369 <amitchaman369@gmail.com>
2023-03-03 16:53:07 +0530

    changes done

---
+++ ChatApplication/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+server.listen(port , () => {
+    console.log(` Server is up on port ${port}!`);
+});
